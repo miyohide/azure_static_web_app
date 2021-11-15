f331cad7e7c0cb1b5d62b66df39a26aca48084e6
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-15 20:30:28 +0900

    move state to Board component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,17 +4,10 @@ import './index.css';
 
 // 正方形のマス目を表すReactコンポーネント
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'}) }>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick() }>
+        {this.props.value}
       </button>
     );
   }
@@ -22,8 +15,21 @@ class Square extends React.Component {
 
 // 盤面を表すReactコンポーネント
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
