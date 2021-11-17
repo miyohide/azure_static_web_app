021e49b9509f38dc0b5eecbdd88429f8c124f015
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-17 21:09:16 +0900

    add bootstrap style

--- posts/src/components/Header.js
+++ posts/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function Header() {
   return (
     <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Posts!!!</Navbar.Brand>
+        </Container>
+      </Navbar>
       ヘッダー
     </div>
   );
