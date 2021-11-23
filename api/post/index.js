2ad722eb0e8b42fc5a8d42d1675d7c1cd739aee3
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-23 14:41:39 +0900

    implement api/post

--- api/post/index.js
+++ api/post/index.js
@@ -1,13 +1,13 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: [
+            {id: 1, title: "title1", body: "body1"},
+            {id: 2, title: "title2", body: "body2"},
+            {id: 3, title: "title3", body: "body3"},
+        ]
+    }
+};
