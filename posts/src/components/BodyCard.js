43c32612f8192edce26cac5d2029a6e2557dd580
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-23 16:46:26 +0900

    add comments

--- posts/src/components/BodyCard.js
+++ posts/src/components/BodyCard.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 function BodyCard(props) {
+  // 分割代入。オブジェクト（props）の各プロパティに対応した
+  // 変数に代入する。
+  // see. https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
   const { title, body } = props;
 
   return (
